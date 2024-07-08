2dd38036cbdca19fa9454f914e2669d812fc9500
davidhcodes <dhanlyg1@gmail.com>
2024-07-08 17:08:47 +0100

    Fixes dotenv to be compatible for react native

--- fe-popcorner/config/firebase.js
+++ fe-popcorner/config/firebase.js
@@ -2,21 +2,32 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import Constants from "expo-constants";
-import { config } from "dotenv";
+// import Constants from "expo-constants";
+// import { config } from "@env";
+// import * as env from "@env";
+
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_DATABASE_URL,
+} from "@env";
 
 // Load environment variables from .env file
-config();
+// config();
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: Constants.expoConfig.apiKey,
-  authDomain: Constants.expoConfig.authDomain,
-  projectId: Constants.expoConfig.projectId,
-  storageBucket: Constants.expoConfig.storageBucket,
-  messagingSenderId: Constants.expoConfig.messagingSenderId,
-  appId: Constants.expoConfig.appId,
-  databaseURL: Constants.expoConfig.databaseURL,
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  databaseURL: FIREBASE_DATABASE_URL,
 };
 
 // Initialize Firebase
