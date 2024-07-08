07de0b3bb6e4ba2570eaa247a21ba8d4636774ae
davidhcodes <dhanlyg1@gmail.com>
2024-07-08 16:40:32 +0100

    Revert "Updates the firebase configuration"
    This reverts commit b921a6a9a9af3c78c2a3e14e48e7551a897f09f3.

--- fe-popcorner/app/(tabs)/CommunityDetails.jsx
+++ fe-popcorner/app/(tabs)/CommunityDetails.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useState } from "react";
 import {
   View,
@@ -10,7 +11,7 @@ import {
   TextInput,
   Button,
 } from "react-native";
-export default function CommunityDetails({ route }) {
+export default function CommunityDetails({ route, user }) {
   const { community } = route.params;
   const [selectedTab, setSelectedTab] = useState("posts");
   const [comments, setComments] = useState({});
@@ -42,8 +43,13 @@ export default function CommunityDetails({ route }) {
     console.log("Confirmed attendance for", event);
   };
   const handleCreatePost = () => {
+    console.log("The value of communitiies is ", { community });
     if (newPost.title.trim() && newPost.body.trim()) {
-      community.posts.push({ ...newPost, author: "CurrentUser" });
+      // community.posts.push({ ...newPost, author: "CurrentUser" });
+      axios.post(
+        `https://popcorner.vercel.app/communities/${community}/posts`,
+        { ...newPost }
+      );
       setNewPost({ title: "", body: "" });
     }
   };
@@ -139,7 +145,9 @@ export default function CommunityDetails({ route }) {
             style={[styles.input, styles.textArea]}
             placeholder="Post Body"
             value={newPost.body}
-            onChangeText={(text) => setNewPost({ ...newPost, body: text })}
+            onChangeText={(text) =>
+              setNewPost({ ...newPost, body: text, author: "Charley" })
+            }
             multiline
           />
           <Button title="Create Post" onPress={handleCreatePost} />
