24d8d63becb3a85c010ba163510126c9685569dc
adamTheWordUnderscoreWillis <adamthewordunderscorewillis@gmail.com>
2024-07-10 14:34:14 +0100

    provdies more professional logo for sign in/ log in page

--- fe-popcorner/screens/newSignUp.js
+++ fe-popcorner/screens/newSignUp.js
@@ -14,7 +14,7 @@ import {
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../config/firebase";
 const backImage = require("../assets/backImage.png");
-const popcorn = require("../assets/popcorn.png");
+const popcorn = require("../assets/popcornerLogo-Large.png");
 
 export default function Signup({
   navigation,
