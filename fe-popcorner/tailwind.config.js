908ff3a052fca06cc124d5aee93f1f105d51b78d
davidhcodes <dhanlyg1@gmail.com>
2024-07-11 10:40:22 +0100

    Updates the styling on the user profile page

--- fe-popcorner/tailwind.config.js
+++ fe-popcorner/tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      hotPink: "#F2055C",
+      White: "#EEEEEE",
+      BlackBackground: "#333",
+      lightGrey: "#474747",
+    },
   },
   plugins: [],
 };
